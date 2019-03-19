da19b169c9b386e7f4b28704af76f71e062d1acd
João Patrocínio <jpatrocinio5436@hotmail.com>
2019-03-19 21:21:33 +0000

    pagination and refactoring movies endpoint queries

--- routes/filme.js
+++ routes/filme.js
@@ -4,74 +4,94 @@ const bcrypt = require('bcryptjs');
 const router= express.Router();
 
 router.get('/', function(req, res) {
-    pool.getConnection(function (err, connection) {
-        if (err) return res.status(500).send({
+    pool.query('SELECT filme_id, filme_title, filme_poster FROM filme', function (error, results, fields) {
+        if (error) return res.status(500).send({
             status: 500,
             response: 'Database error. Please try again.'
         });
-        connection.query('SELECT filme_id, filme_title, filme_poster FROM filme', function (error, results, fields) {
+
+        if (!results[0]) {
+            return res.status(404).send({
+                status: 404,
+                response: 'Movies not found.'
+            });
+        }
+
+        return res.status(200).send({
+            status: 200,
+            response: results
+        });
+    });
+})
+
+router.get('/:size/:page', function (req, res) {
+    var size = req.params.size;
+    var page = req.params.page;
+    if (size >= 1 && page >= 1) {
+        pool.query('SELECT filme_id, filme_title, filme_poster FROM filme LIMIT ? OFFSET ?', [parseInt(size), (page - 1) * size], function(error, results, fields) {
+            if (error) return res.status(500).send({
+                status: 500,
+                response: "Database error. Please try again."
+            });
+
             if (!results[0]) {
                 return res.status(404).send({
                     status: 404,
-                    response: 'Movie not found.'
+                    response: 'Movies not found.'
                 });
             }
-
             return res.status(200).send({
                 status: 200,
                 response: results
             });
-
         });
-        connection.release();
-    });
-})
+    } else {
+        return res.status(400).send({
+            status: 400,
+            response: "Incorrect query, result size and number page must be >= 1"
+        });
+    }
+});
 
 router.get('/:filme_id', function(req, res) {
-    pool.getConnection(function (err, connection) {
-        if (err) return res.status(500).send({
+    pool.query('SELECT filme.*, realizador.realizador_nome FROM filme LEFT JOIN realizador ON filme.filme_realizador_id = realizador.realizador_id WHERE filme_id = ?', [req.params.filme_id], function (error, results, fields) {
+        if (error) return res.status(500).send({
             status: 500,
             response: 'Database error. Please try again.'
         });
-        connection.query('SELECT filme.*, realizador.realizador_nome FROM filme LEFT JOIN realizador ON filme.filme_realizador_id = realizador.realizador_id WHERE filme_id = ?', [req.params.filme_id], function (error, results, fields) {
-            if (!results[0]) {
-                return res.status(404).send({
-                    status: 404,
-                    response: 'Movie not found.'
-                });
-            }
 
-            return res.status(200).send({
-                status: 200,
-                response: results[0]
+        if (!results[0]) {
+            return res.status(404).send({
+                status: 404,
+                response: 'Movie not found.'
             });
+        }
 
+        return res.status(200).send({
+            status: 200,
+            response: results[0]
         });
-        connection.release();
     });
 })
 
 router.get('/atores/:filme_id', function (req, res) {
-    pool.getConnection(function (err, connection) {
-        if (err) return res.status(500).send({
+    connection.query('SELECT filme_id, ator_id, ator_nome FROM filme LEFT JOIN filme_ator ON filme_ator.filme_ator_filme_id = filme.filme_id LEFT JOIN ator ON ator.ator_id = filme_ator.filme_ator_ator_id WHERE filme.filme_id = ?', [req.params.filme_id], function (error, results, fields) {
+        if (error) return res.status(500).send({
             status: 500,
             response: 'Database error. Please try again.'
         });
-        connection.query('SELECT filme_id, ator_id, ator_nome FROM filme LEFT JOIN filme_ator ON filme_ator.filme_ator_filme_id = filme.filme_id LEFT JOIN ator ON ator.ator_id = filme_ator.filme_ator_ator_id WHERE filme.filme_id = ?', [req.params.filme_id], function (error, results, fields) {
-            if (!results[0]) {
-                return res.status(404).send({
-                    status: 404,
-                    response: 'Movie not found.'
-                });
-            }
 
-            return res.status(200).send({
-                status: 200,
-                response: results
+        if (!results[0]) {
+            return res.status(404).send({
+                status: 404,
+                response: 'Movie not found.'
             });
+        }
 
+        return res.status(200).send({
+            status: 200,
+            response: results
         });
-        connection.release();
     });
 })
 
