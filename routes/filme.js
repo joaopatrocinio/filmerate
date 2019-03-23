9c999e3c50224f0cd8be3327514511cd62580d8d
João Patrocínio <jpatrocinio5436@hotmail.com>
2019-03-23 18:19:57 +0000

    added pagination to some search endpoints

--- routes/filme.js
+++ routes/filme.js
@@ -95,25 +95,34 @@ router.get('/atores/:filme_id', function (req, res) {
     });
 })
 
-router.get('/search/:query', function (req, res) {
-    pool.query("SELECT filme_id, filme_title, filme_poster FROM filme WHERE filme_title LIKE '%" + this.escape(req.params.query) + "%'", function (error, results, fields) {
-        if (error) return res.status(500).send({
-            status: 500,
-            response: error
-        });
-
-        if (!results[0]) {
-            return res.status(404).send({
-                status: 404,
-                response: 'Movie not found.'
+router.get('/search/:query/size/:size/page/:page', function (req, res) {
+    var size = req.params.size;
+    var page = req.params.page;
+    if (size >= 1 && page >= 1) {
+        pool.query("SELECT filme_id, filme_title, filme_poster FROM filme WHERE filme_title LIKE '%" + this.escape(req.params.query) + "%' LIMIT ? OFFSET ?", [parseInt(size), (page - 1) * size], function (error, results, fields) {
+            if (error) return res.status(500).send({
+                status: 500,
+                response: error
             });
-        }
 
-        return res.status(200).send({
-            status: 200,
-            response: results
+            if (!results[0]) {
+                return res.status(404).send({
+                    status: 404,
+                    response: 'Movie not found.'
+                });
+            }
+
+            return res.status(200).send({
+                status: 200,
+                response: results
+            });
         });
-    });
+    } else {
+        return res.status(400).send({
+            status: 400,
+            response: "Incorrect query, result size and number page must be >= 1"
+        });
+    }
 });
 
 module.exports = router;
--- routes/genero.js
+++ routes/genero.js
@@ -20,26 +20,35 @@ router.get('/', function(req, res) {
     });
 })
 
-router.get('/:genero_id', function (req, res) {
-    pool.query('SELECT filme.filme_id, filme_genero_genero_id, filme.filme_title, filme.filme_poster FROM filme_genero INNER JOIN filme ON filme_genero_filme_id = filme.filme_id WHERE filme_genero_genero_id = ? GROUP BY filme.filme_id', [req.params.genero_id], function (error, results, fields) {
-        if (error) {
-            return res.status(500).send({
-                status: 500,
-                respose: error
-            });
-        }
-        if (!results[0]) {
-            return res.status(404).send({
-                status: 404,
-                response: 'Movies not found.'
-            });
-        }
+router.get('/:genero_id/size/:size/page/:page', function (req, res) {
+    var size = req.params.size;
+    var page = req.params.page;
+    if (size >= 1 && page >= 1) {
+        pool.query('SELECT filme.filme_id, filme_genero_genero_id, filme.filme_title, filme.filme_poster FROM filme_genero INNER JOIN filme ON filme_genero_filme_id = filme.filme_id WHERE filme_genero_genero_id = ? GROUP BY filme.filme_id LIMIT ? OFFSET ?', [req.params.genero_id, parseInt(size), (page - 1) * size], function (error, results, fields) {
+            if (error) {
+                return res.status(500).send({
+                    status: 500,
+                    respose: error
+                });
+            }
+            if (!results[0]) {
+                return res.status(404).send({
+                    status: 404,
+                    response: 'Movies not found.'
+                });
+            }
 
-        return res.status(200).send({
-            status: 200,
-            response: results
+            return res.status(200).send({
+                status: 200,
+                response: results
+            });
         });
-    });
+    } else {
+        return res.status(400).send({
+            status: 400,
+            response: "Incorrect query, result size and number page must be >= 1"
+        });
+    }
 })
 
 module.exports = router;
