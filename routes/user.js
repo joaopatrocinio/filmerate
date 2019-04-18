3fe1fa91a347ee678d127ac17b9c46b4ccfb0026
João Patrocínio <jpatrocinio5436@hotmail.com>
2019-04-19 00:08:38 +0100

    user endpoint for fresh reviews

--- routes/user.js
+++ routes/user.js
@@ -254,4 +254,21 @@ router.get('/reviews', function (req, res) {
     })
 })
 
+router.get('/reviews/fresh', function(req, res) {
+    pool.query("SELECT sum(CASE filme_classificacao_score_updown WHEN '1' THEN 1 ELSE 0 END) AS 'likes', sum(CASE filme_classificacao_score_updown WHEN '0' THEN 1 ELSE 0 END ) AS 'dislikes', sum(CASE filme_classificacao_score_updown WHEN '1' AND filme_classificacao_score_user_id = ? THEN 1 ELSE 0 END ) AS 'liked', sum(CASE filme_classificacao_score_updown WHEN '0' AND filme_classificacao_score_user_id = ? THEN 1 ELSE 0 END ) AS 'disliked', filme_title, user_firstname, user_lastname, filme_poster, filme_classificacao.* FROM filme_classificacao LEFT JOIN filme ON filme_id = filme_classificacao_filme_id LEFT JOIN filme_classificacao_score ON filme_classificacao_score_filme_classificacao_id = filme_classificacao_id LEFT JOIN user ON filme_classificacao_user_id = user_id GROUP BY filme_classificacao_id ORDER BY filme_classificacao_data DESC LIMIT 5 OFFSET 0", [user_id, user_id], function (error, results, fields) {
+        if (!results[0]) {
+            return res.status(404).send({
+                status: 404,
+                response: 'Reviews not found.'
+            });
+        }
+
+        return res.status(200).send({
+            status: 200,
+            response: results
+        });
+
+    });
+})
+
 module.exports = router;
