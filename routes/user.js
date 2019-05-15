7493a92206de6322f34caa40d0113ee98f9529ac
João Patrocínio <jpatrocinio5436@hotmail.com>
2019-05-15 15:28:07 +0100

    users reviews with auth

--- routes/user.js
+++ routes/user.js
@@ -390,4 +390,27 @@ router.post('/profile/edit', function (req, res) {
     }
 })
 
+router.get('/reviews/users/:user_id', function (req, res) {
+    pool.query("SELECT sum(CASE filme_classificacao_score_updown WHEN '1' THEN 1 ELSE 0 END) AS 'likes', sum(CASE filme_classificacao_score_updown WHEN '0' THEN 1 ELSE 0 END ) AS 'dislikes', sum(CASE WHEN filme_classificacao_score_updown = '1' AND filme_classificacao_score_user_id = ? THEN 1 ELSE 0 END ) AS 'liked', sum(CASE WHEN filme_classificacao_score_updown = '0' AND filme_classificacao_score_user_id = ? THEN 1 ELSE 0 END ) AS 'disliked', filme_title, user_firstname, user_lastname, filme_poster, filme_classificacao.* FROM filme_classificacao LEFT JOIN filme ON filme_id = filme_classificacao_filme_id LEFT JOIN filme_classificacao_score ON filme_classificacao_score_filme_classificacao_id = filme_classificacao_id LEFT JOIN user ON filme_classificacao_user_id = user_id WHERE filme_classificacao_user_id = ? GROUP BY filme_classificacao_id ORDER BY filme_classificacao_data DESC", [user_id, user_id, req.params.user_id], function (error, results, fields) {
+        if (error) {
+            return res.status(500).send({
+                status: 500,
+                response: "Database error. Please try again."
+            });
+        }
+
+        if (results[0]) {
+            return res.status(200).send({
+                status: 200,
+                response: results
+            });
+        } else {
+            return res.status(404).send({
+                status: 404,
+                response: "Reviews not found."
+            })
+        }
+    });
+})
+
 module.exports = router;
