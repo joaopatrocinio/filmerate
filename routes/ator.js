e2cf43d9b945b66171b05b3c249659111ff85daa
João Patrocínio <jpatrocinio5436@hotmail.com>
2019-05-23 20:27:29 +0100

    list actor's movies

--- routes/ator.js
+++ routes/ator.js
@@ -37,4 +37,25 @@ router.get('/:ator_id', function(req, res) {
     });
 })
 
+router.get('/:ator_id/filmes', function(req, res) {
+    if (req.params.ator_id) {
+        pool.query('SELECT filme_id, filme_title, filme_poster FROM filme_ator LEFT JOIN filme ON filme.filme_id = filme_ator_filme_id WHERE filme_ator_ator_id = ?', [req.params.ator_id], function (error, results) {
+            if (error) return res.status(500).send({
+                status: 500,
+                response: "Database error. Please try again."
+            })
+
+            return res.status(200).send({
+                status: 200,
+                response: results
+            })
+        })
+    } else {
+        return res.status(400).send({
+            status: 400,
+            response: "Please include all necessary data in the request."
+        })
+    }
+})
+
 module.exports = router;
