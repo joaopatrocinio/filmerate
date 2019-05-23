9a979feb905d30ce88e8e79ce26f0219bf4e95f3
João Patrocínio <jpatrocinio5436@hotmail.com>
2019-05-23 19:49:52 +0100

    show director's movies

--- routes/realizador.js
+++ routes/realizador.js
@@ -37,4 +37,25 @@ router.get('/:realizador_id', function(req, res) {
     });
 })
 
+router.get('/:realizador_id/filmes', function (req, res) {
+    if (req.params.realizador_id) {
+        pool.query('SELECT filme_id, filme_title, filme_poster FROM filme WHERE filme_realizador_id = ?', [req.params.realizador_id], function (error, results) {
+            if (error) return res.status(500).send({
+                status: 500,
+                response: "Database error. Please try again."
+            })
+
+            return res.status(200).send({
+                status: 200,
+                response: results
+            })
+        })
+    } else {
+        return res.status(400).send({
+            status: 400,
+            response: "Please include all necessary data in the request."
+        })
+    }
+})
+
 module.exports = router;
